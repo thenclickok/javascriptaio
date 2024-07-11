ac49516d2a7204a8cf3454c3632e86729d8e189b
Jessica Azzopardi <vegan4life@live.co.uk>
2024-07-11 19:17:59 +0100

    Learning more about Svelte

---
+++ svelte/my-svelte-app/src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import TransitionTest from "./lib/TransitionTest.svelte";
+  let newPost = '';
+  let posts = [];
+  function addPost(){
+    posts = [...posts,newPost];
+  }
+</script>
+
+<main>
+  <h1>Soliloquy</h1>
+  <h2>Social media without the sharing</h2>
+  <label>Talk to yourself: <input bind:value={newPost} type="text"/></label>
+  <button on:click={addPost}>Post It!</button>
+  <div>
+    {#each posts as post}
+      <div>{post}</div>
+    {/each}
+  </div>
+  <div>
+  <TransitionTest />
+  </div>
+</main>
+
+<style>
+  
+</style>
