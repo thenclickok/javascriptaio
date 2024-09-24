d0cc81057b144d418a0f52cbeefb0f060eacf0bc
Jessica Azzopardi <vegan4life@live.co.uk>
2024-09-24 11:28:04 +0100

    More Node.js learning

---
+++ node.js/Listing 070501.mjs
@@ -0,0 +1,13 @@
+import http from "http";
+
+const app = http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("Hello World!");
+    res.end();
+
+    //res.end('<h1>Hello World</h1>');
+  })
+  .listen(3000, () => {
+    console.log("Server running at http://localhost:3000/");
+  });
